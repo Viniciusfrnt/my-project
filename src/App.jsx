e915192c5bd1f48a78ec06b601ac32b8ee8aea16
Vinicius Almeida <119829401+Viniciusfrnt@users.noreply.github.com>
2023-08-04 23:04:30 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,13 @@ function App() {
           </div>
          
           <div className="bg-blue-950 rounded-lg p-4">
-            <h1 className="text-white text-2xl mb-2">FULL STACK DEVELOPMENT</h1>
+            <h1 className="text-white text-2xl mb-2">DATA ANALYTICS</h1>
             <p className="text-gray-200 mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eveniet facere quos, deserunt modi aut dicta sed quia labore earum perferendis ea placeat voluptatum, minus consequatur officia voluptates, ab magnam!</p>
             <button className="bg-gray-300 text-blue-950 text-md mt-20 p-2 rounded-md hover:bg-blue-600 hover:text-gray-200 transition-all">Saiba Mais</button>
           </div>
          
           <div className="bg-blue-950 rounded-lg p-4">
-            <h1 className="text-white text-2xl mb-2">FULL STACK DEVELOPMENT</h1>
+            <h1 className="text-white text-2xl mb-2">DIGITAL PRODUCT (UX/UI)</h1>
             <p className="text-gray-200 mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eveniet facere quos, deserunt modi aut dicta sed quia labore earum perferendis ea placeat voluptatum, minus consequatur officia voluptates, ab magnam!</p>
             <button className="bg-gray-300 text-blue-950 text-md mt-20 p-2 rounded-md hover:bg-blue-600 hover:text-gray-200 transition-all">Saiba Mais</button>
           </div>
